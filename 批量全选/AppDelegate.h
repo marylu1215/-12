d5ca474039137952e756e8b863917a34318c349b
mary lu <xhkj@xhkjdemac-mini.local>
2018-05-05 10:18:59 +0800

    Initial Commit

---
+++ 批量全选/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  批量全选
+//
+//  Created by xhkj on 2018/5/5.
+//  Copyright © 2018年 JDKJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 批量全选/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  批量全选
+//
+//  Created by xhkj on 2018/5/5.
+//  Copyright © 2018年 JDKJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
